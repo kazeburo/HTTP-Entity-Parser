b35e74f7f20af6d355735c61f8feb4bfb99c412b
Masahiro Nagano <kazeburo@gmail.com>
2014-02-24 22:53:48 +0900

    update benchmark

--- eg/bench.pl
+++ eg/bench.pl
@@ -7,9 +7,7 @@ use HTTP::Body;
 use Benchmark qw/:all/;
 
 my $content1 = 'xxx=hogehoge&yyy=aaaaaaaaaaaaaaaaaaaaa';
-
 my $content2 = 'xxx=hogehoge&yyy=aaaaaaaaaaaaaaaaaaaaa&%E6%97%A5%E6%9C%AC%E8%AA%9E=%E3%81%AB%E3%81%BB%E3%82%93%E3%81%94&%E3%81%BB%E3%81%92%E3%81%BB%E3%81%92=%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C';
-
 my $content3 = join '&', map { "$_=%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C%E3%81%B5%E3%81%8C" } 'A'..'R';
 
 my $parser = HTTP::Entity::Parser->new;
@@ -43,26 +41,28 @@ __END__
 ## content length => 38
 
 Benchmark: running http_body, http_entity for at least 1 CPU seconds...
- http_body:  1 wallclock secs ( 1.02 usr +  0.00 sys =  1.02 CPU) @ 34132.35/s (n=34815)
+ http_body:  1 wallclock secs ( 1.07 usr +  0.00 sys =  1.07 CPU) @ 33494.39/s (n=35839)
 http_entity:  1 wallclock secs ( 1.08 usr +  0.00 sys =  1.08 CPU) @ 79643.52/s (n=86015)
                Rate   http_body http_entity
-http_body   34132/s          --        -57%
-http_entity 79644/s        133%          --
+http_body   33494/s          --        -58%
+http_entity 79644/s        138%          --
 
 ## content length => 177
 
 Benchmark: running http_body, http_entity for at least 1 CPU seconds...
- http_body:  2 wallclock secs ( 1.17 usr +  0.00 sys =  1.17 CPU) @ 14137.61/s (n=16541)
-http_entity:  1 wallclock secs ( 1.06 usr +  0.00 sys =  1.06 CPU) @ 67621.70/s (n=71679)
+ http_body:  2 wallclock secs ( 1.01 usr +  0.00 sys =  1.01 CPU) @ 14193.07/s (n=14335)
+http_entity:  1 wallclock secs ( 1.01 usr +  0.00 sys =  1.01 CPU) @ 70969.31/s (n=71679)
                Rate   http_body http_entity
-http_body   14138/s          --        -79%
-http_entity 67622/s        378%          --
+http_body   14193/s          --        -80%
+http_entity 70969/s        400%          --
 
 ## content length => 1997
 
 Benchmark: running http_body, http_entity for at least 1 CPU seconds...
- http_body:  1 wallclock secs ( 1.09 usr +  0.00 sys =  1.09 CPU) @ 2054.13/s (n=2239)
-http_entity:  2 wallclock secs ( 1.13 usr +  0.00 sys =  1.13 CPU) @ 29276.99/s (n=33083)
+ http_body:  1 wallclock secs ( 1.06 usr +  0.00 sys =  1.06 CPU) @ 1950.00/s (n=2067)
+http_entity:  1 wallclock secs ( 1.10 usr +  0.00 sys =  1.10 CPU) @ 35543.64/s (n=39098)
                Rate   http_body http_entity
-http_body    2054/s          --        -93%
-http_entity 29277/s       1325%          --
+http_body    1950/s          --        -95%
+http_entity 35544/s       1723%          --
+
+
